72e7dd358b71c771813dca0196dc1645584c3203
JoyLynn <joylynnious@gmail.com>
2015-08-24 17:10:20 +0300

    added a method that list all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+	def index
+    @articles = Article.all
+  	end
+
 	def show
     @article = Article.find(params[:id])
     end
@@ -11,12 +15,10 @@ class ArticlesController < ApplicationController
 	def create
 		# render plain: params[:article].inspect
 
-		@article = Article.new(params[:article])
+		@article = Article.new(params.require(:article).permit(:title, :text))
  
 		@article.save
 		redirect_to @article
-
-		# @article = Article.new(params.require(:article).permit(:title, :text))
 
 	end
 
@@ -24,6 +26,5 @@ class ArticlesController < ApplicationController
   	def article_params
     	params.require(:article).permit(:title, :text)
   	end
-
 	
 end
